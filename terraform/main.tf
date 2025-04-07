452893dd0f7d3628cc75de7d061bbdc65a7e28ad
rise22tz <rise22tz@student.ju.se>
2025-04-07 13:27:53 +0200

    uppdaterade runner och lade till dns-servrar

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,7 +26,7 @@ data "vsphere_compute_cluster" "cluster" {
 
 data "vsphere_virtual_machine" "template" {
   # VM som används som template och blir klonad
-  name          = var.ubuntu_name
+  name          = "jammy-server-cloudimg-amd64"
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
 
@@ -50,6 +50,12 @@ resource "vsphere_folder" "vpn" {
 
 resource "vsphere_folder" "runner" {
   path          = "Runners"
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
+
+resource "vsphere_folder" "dns" {
+  path          = "DNS"
   type          = "vm"
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
@@ -118,8 +124,8 @@ resource "vsphere_virtual_machine" "runner" {
   name             = "runner-${count.index + 1}"
   resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
   datastore_id     = data.vsphere_datastore.datastore.id
-  num_cpus         = 1
-  memory           = 1024
+  num_cpus         = 2
+  memory           = 2048
   folder           = vsphere_folder.runner.path
   network_interface {
     network_id = data.vsphere_network.mgmt.id
@@ -161,6 +167,63 @@ resource "vsphere_virtual_machine" "runner" {
 
       linux_options {
         host_name = "runner-${count.index + 1}"
+        domain    = "virt.local"
+
+
+      }
+    }
+  }
+}
+
+# Publik DNS
+resource "vsphere_virtual_machine" "nameserver" {
+  count            = 2
+  name             = "ns-${count.index + 1}"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
+  num_cpus         = 1
+  memory           = 1024
+  folder           = vsphere_folder.dns.path
+  network_interface {
+    network_id = data.vsphere_network.dmz.id
+  }
+  disk {
+    label            = "Hard Disk 1"
+    size             = 16
+    thin_provisioned = false
+  }
+
+  cdrom {
+    client_device = true
+  }
+  vapp {
+    properties = {
+      "hostname"    = "ns-${count.index + 1}"
+      "public-keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCnq5ZGGA2Fa17jaWNrBdcvWfhVqUqi6xTksaXJSDvM"
+      "instance-id" = "ns-${count.index + 1}"
+    }
+  }
+
+  # Måste vara där
+  guest_id = "ubuntu64Guest"
+
+  clone {
+    # Referens till templaten som deklarerades tidigare
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+
+      network_interface {
+        ipv4_address = "10.200.200.1${count.index + 1}"
+        ipv4_netmask = "24"
+
+      }
+
+      ipv4_gateway    = "10.200.200.254"
+      dns_server_list = ["10.200.200.254"]
+      dns_suffix_list = ["virt.local"]
+
+      linux_options {
+        host_name = "ns-${count.index + 1}"
         domain    = "virt.local"
 
 
