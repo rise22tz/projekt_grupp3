37a89b6d5e5cd6348f47a2911cb6380addbc6dfa
rise22tz <rise22tz@student.ju.se>
2025-04-26 20:59:30 +0200

    big commit med kubernetes

--- terraform/main.tf
+++ terraform/main.tf
@@ -85,7 +85,17 @@ resource "vsphere_folder" "mail" {
   type          = "vm"
   datacenter_id = data.vsphere_datacenter.datacenter.id
 }
+resource "vsphere_folder" "monitor" {
+  path          = "Monitor"
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
 
+resource "vsphere_folder" "mgmt" {
+  path          = "Management"
+  type          = "vm"
+  datacenter_id = data.vsphere_datacenter.datacenter.id
+}
 
 
 # VPN
@@ -156,7 +166,7 @@ resource "vsphere_virtual_machine" "runner" {
   memory           = 2048
   folder           = vsphere_folder.runner.path
   network_interface {
-    network_id = data.vsphere_network.servers.id
+    network_id = data.vsphere_network.mgmt.id
   }
   disk {
     label            = "Hard Disk 1"
@@ -655,7 +665,116 @@ resource "vsphere_virtual_machine" "test-yolo" {
   }
 }
 
+resource "vsphere_virtual_machine" "monitor" {
+  count            = 1
+  name             = "monitor-${count.index + 1}"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = count.index % 2 == 0 ? data.vsphere_datastore.datastore1.id : data.vsphere_datastore.datastore2.id
+  folder           = vsphere_folder.monitor.path
+  num_cpus         = 2
+  memory           = 4096
+  network_interface {
+    network_id = data.vsphere_network.mgmt.id
+  }
+  disk {
+    label            = "Hard Disk 1"
+    size             = 16
+    thin_provisioned = false
+  }
 
+  cdrom {
+    client_device = true
+  }
+  vapp {
+    properties = {
+      "hostname"    = "monitor-${count.index + 1}"
+      "public-keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCnq5ZGGA2Fa17jaWNrBdcvWfhVqUqi6xTksaXJSDvM"
+      "instance-id" = "monitor-${count.index + 1}"
+    }
+  }
+
+  # Måste vara där
+  guest_id = "ubuntu64Guest"
+
+  clone {
+    # Referens till templaten som deklarerades tidigare
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+
+      network_interface {
+        ipv4_address = "10.200.100.15${count.index + 1}"
+        ipv4_netmask = "24"
+
+      }
+
+      ipv4_gateway    = "10.200.100.1"
+      dns_server_list = ["10.200.50.11", "10.200.50.12"]
+      dns_suffix_list = ["lan.grupp3.dnlab.se"]
+
+      linux_options {
+        host_name = "monitor-${count.index + 1}"
+        domain    = "grupp3.dnlab.se"
+
+
+      }
+    }
+  }
+}
+resource "vsphere_virtual_machine" "rancher" {
+  count            = 1
+  name             = "rancher-${count.index + 1}"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = count.index % 2 == 0 ? data.vsphere_datastore.datastore1.id : data.vsphere_datastore.datastore2.id
+  folder           = vsphere_folder.mgmt.path
+  num_cpus         = 2
+  memory           = 4096
+  network_interface {
+    network_id = data.vsphere_network.mgmt.id
+  }
+  disk {
+    label            = "Hard Disk 1"
+    size             = 16
+    thin_provisioned = false
+  }
+
+  cdrom {
+    client_device = true
+  }
+  vapp {
+    properties = {
+      "hostname"    = "rancher-${count.index + 1}"
+      "public-keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCnq5ZGGA2Fa17jaWNrBdcvWfhVqUqi6xTksaXJSDvM"
+      "instance-id" = "rancher-${count.index + 1}"
+    }
+  }
+
+  # Måste vara där
+  guest_id = "ubuntu64Guest"
+
+  clone {
+    # Referens till templaten som deklarerades tidigare
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+
+      network_interface {
+        ipv4_address = "10.200.100.8${count.index + 1}"
+        ipv4_netmask = "24"
+
+      }
+
+      ipv4_gateway    = "10.200.100.1"
+      dns_server_list = ["10.200.50.11", "10.200.50.12"]
+      dns_suffix_list = ["lan.grupp3.dnlab.se"]
+
+      linux_options {
+        host_name = "rancher-${count.index + 1}"
+        domain    = "grupp3.dnlab.se"
+
+
+      }
+    }
+  }
+}
 
 locals {
   vm_groups = {
