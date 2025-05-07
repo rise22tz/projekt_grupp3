a2e8c4a9c809ccef0ab3e16c0ab2d3e6c6b9c80b
rise22tz <rise22tz@student.ju.se>
2025-05-07 08:31:16 +0200

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -775,61 +775,7 @@ resource "vsphere_virtual_machine" "rancher" {
     }
   }
 }
-resource "vsphere_virtual_machine" "ids" {
-  count            = 1
-  name             = "ids-${count.index + 1}"
-  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-  datastore_id     = count.index % 2 == 0 ? data.vsphere_datastore.datastore1.id : data.vsphere_datastore.datastore2.id
-  folder           = vsphere_folder.mgmt.path
-  num_cpus         = 2
-  memory           = 4096
-  network_interface {
-    network_id = data.vsphere_network.mgmt.id
-  }
-  disk {
-    label            = "Hard Disk 1"
-    size             = 16
-    thin_provisioned = false
-  }
 
-  cdrom {
-    client_device = true
-  }
-  vapp {
-    properties = {
-      "hostname"    = "ids-${count.index + 1}"
-      "public-keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCnq5ZGGA2Fa17jaWNrBdcvWfhVqUqi6xTksaXJSDvM"
-      "instance-id" = "ids-${count.index + 1}"
-    }
-  }
-
-  # Måste vara där
-  guest_id = "ubuntu64Guest"
-
-  clone {
-    # Referens till templaten som deklarerades tidigare
-    template_uuid = data.vsphere_virtual_machine.template.id
-    customize {
-
-      network_interface {
-        ipv4_address = "10.200.100.9${count.index + 1}"
-        ipv4_netmask = "24"
-
-      }
-
-      ipv4_gateway    = "10.200.100.1"
-      dns_server_list = ["10.200.50.11", "10.200.50.12"]
-      dns_suffix_list = ["lan.grupp3.dnlab.se"]
-
-      linux_options {
-        host_name = "ids-${count.index + 1}"
-        domain    = "grupp3.dnlab.se"
-
-
-      }
-    }
-  }
-}
 resource "vsphere_virtual_machine" "borg" {
   count            = 1
   name             = "borg-${count.index + 1}"
@@ -878,6 +824,61 @@ resource "vsphere_virtual_machine" "borg" {
 
       linux_options {
         host_name = "borg-${count.index + 1}"
+        domain    = "lan.grupp3.dnlab.se"
+
+
+      }
+    }
+  }
+}
+resource "vsphere_virtual_machine" "sql" {
+  count            = 1
+  name             = "sql-${count.index + 1}"
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = count.index % 2 == 0 ? data.vsphere_datastore.datastore1.id : data.vsphere_datastore.datastore2.id
+  folder           = vsphere_folder.mgmt.path
+  num_cpus         = 1
+  memory           = 2048
+  network_interface {
+    network_id = data.vsphere_network.servers.id
+  }
+  disk {
+    label            = "Hard Disk 1"
+    size             = 16
+    thin_provisioned = false
+  }
+
+  cdrom {
+    client_device = true
+  }
+  vapp {
+    properties = {
+      "hostname"    = "sql-${count.index + 1}"
+      "public-keys" = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKCnq5ZGGA2Fa17jaWNrBdcvWfhVqUqi6xTksaXJSDvM"
+      "instance-id" = "sql-${count.index + 1}"
+    }
+  }
+
+  # Måste vara där
+  guest_id = "ubuntu64Guest"
+
+  clone {
+    # Referens till templaten som deklarerades tidigare
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+
+      network_interface {
+        ipv4_address = "10.200.50.5${count.index + 1}"
+        ipv4_netmask = "24"
+
+      }
+
+      ipv4_gateway    = "10.200.50.1"
+      dns_server_list = ["10.200.50.11", "10.200.50.12"]
+      dns_suffix_list = ["lan.grupp3.dnlab.se"]
+
+      linux_options {
+        host_name = "sql-${count.index + 1}"
         domain    = "lan.grupp3.dnlab.se"
 
 
